9434c64eaf368980afc8336d77c5dcb65e7ac46b
Vic-Chang <s9826111@ulive.pccu.edu.tw>
2022-07-15 23:49:15 +0800

    Download all .ts files

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
+import os
 import requests
 import m3u8
 
 
-def download_ts_files(m3u8_url:str):
+def download_ts_files(m3u8_url: str):
     playlist = m3u8.load(m3u8_url)
+    temp_ts_folder = 'temp_ts'
+    request = requests.session()
+    if not os.path.exists(temp_ts_folder):
+        os.mkdir(temp_ts_folder)
     for item in playlist.segments:
-        print(item.uri)
+        file_name = os.path.basename(item.uri).split("?")[0]
+        with request.get(item.uri, stream=True) as r:
+            r.raise_for_status()
+            with open(os.path.join(temp_ts_folder, file_name), 'wb') as f:
+                for chunk in r.iter_content(chunk_size=1024):
+                    f.write(chunk)
 
 
 if __name__ == '__main__':
-    url = 'https://api.mudvod.tv/play/mud.m3u8/WEB/1.0?ce=473c79abad38cea8e5598e3922847f34e907ddac030cff807f11a526b01c9322d431718768268810b7a7ca24b7650ae43ecb5aabfc4d6d1a0526829eb6f8a308ee66ab29ae403b818c1a61e641580195de6913d4bd00eda158e2125d164c55b179f9cdc1e5031693e1b23d4d75715c8afc04f4848945a8b1&pf=3&uk=50e4f5cc108d6dc0cc3b17c3ecb914d6&rx=14004&expire=1657817183948&ip=118.163.56.205&sign=757e7793aa29544292e5bac07b9d6d12&_ts=1657791983948'
+    url = 'https://api.mudvod.tv/play/mud.m3u8/WEB/1.0?ce=6d657528bb2f55f7020392a18e350cf824adffef259330a6ba1509ff3621ce5f6927aa56b8c7e598847139ca75e82783b6088e5fedf034674fcb0a046246c5ad178e5e6828ef577327c0bb6e1a025b284b9245be9687d66a4d8a14fdbeb2ab4ed08f6bbaa395ee43e5d899e1efe860dcc889f8a570ac9bdc&pf=3&uk=ad552ef8019333d927d0aec6afb2d17d&rx=1898&expire=1657923827778&ip=118.163.56.205&sign=453eaa84866dd979a4a595663c154cec&_ts=1657898627778'
     download_ts_files(url)
