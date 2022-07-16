e8e3498a310bf4fa65f396499f6a6e5111be62db
Vic-Chang <s9826111@ulive.pccu.edu.tw>
2022-07-17 04:17:04 +0800

    Use multithreading  to improve download efficiency

--- app.py
+++ app.py
@@ -1,23 +1,50 @@
 import os
 import requests
 import m3u8
+import queue
+import threading
+
+que = queue.Queue()
+request = requests.session()
+
+temp_ts_folder = 'temp_ts'
+if not os.path.exists(temp_ts_folder):
+    os.mkdir(temp_ts_folder)
 
 
-def download_ts_files(m3u8_url: str):
+def get_all_ts_files_url(m3u8_url: str):
     playlist = m3u8.load(m3u8_url)
-    temp_ts_folder = 'temp_ts'
-    request = requests.session()
-    if not os.path.exists(temp_ts_folder):
-        os.mkdir(temp_ts_folder)
     for item in playlist.segments:
-        file_name = os.path.basename(item.uri).split("?")[0]
-        with request.get(item.uri, stream=True) as r:
+        que.put(item.uri)
+    print('Set all ts files url in que!')
+
+
+def download_ts_file():
+    while que.not_empty:
+        ts_url = que.get()
+        file_name = os.path.basename(ts_url).split("?")[0]
+        with request.get(ts_url, stream=True) as r:
             r.raise_for_status()
             with open(os.path.join(temp_ts_folder, file_name), 'wb') as f:
                 for chunk in r.iter_content(chunk_size=1024):
                     f.write(chunk)
+                print(f'{threading.get_ident()}: Download success! {file_name}')
+
+
+def download_ts_file_job():
+    threads = []
+    for _ in range(10):
+        t = threading.Thread(target=download_ts_file)
+        threads.append(t)
+    for t in threads:
+        t.start()
+    print('All threading are starts!')
+    for t in threads:
+        t.join()
+    print('All threading done!')
 
 
 if __name__ == '__main__':
-    url = 'https://api.mudvod.tv/play/mud.m3u8/WEB/1.0?ce=6d657528bb2f55f7020392a18e350cf824adffef259330a6ba1509ff3621ce5f6927aa56b8c7e598847139ca75e82783b6088e5fedf034674fcb0a046246c5ad178e5e6828ef577327c0bb6e1a025b284b9245be9687d66a4d8a14fdbeb2ab4ed08f6bbaa395ee43e5d899e1efe860dcc889f8a570ac9bdc&pf=3&uk=ad552ef8019333d927d0aec6afb2d17d&rx=1898&expire=1657923827778&ip=118.163.56.205&sign=453eaa84866dd979a4a595663c154cec&_ts=1657898627778'
-    download_ts_files(url)
+    url = 'https://api.mudvod.tv/play/mud.m3u8/WEB/1.0?ce=6d657528bb2f55f7020392a18e350cf824adffef259330a6ba1509ff3621ce5f6927aa56b8c7e598847139ca75e82783b6088e5fedf034674fcb0a046246c5ad178e5e6828ef577327c0bb6e1a025b284b9245be9687d66a4d8a14fdbeb2ab4ed08f6bbaa395ee43e5d899e1efe860dcc889f8a570ac9bdc&pf=3&uk=ad552ef8019333d927d0aec6afb2d17d&rx=17592&expire=1658024604069&ip=118.163.56.205&sign=274f961f7aee1577ebf21dc3389aed65&_ts=1657999404069'
+    get_all_ts_files_url(url)
+    download_ts_file_job()
